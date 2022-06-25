55dcbae0f13cf5f23dda33dbd93b5298b8b2e664
Richard Tae <taerichard@gmail.com>
2022-06-24 20:14:09 -0700

    worked on profile intro, working on project section

--- src/components/HomeComponents/SecondContent.js
+++ src/components/HomeComponents/SecondContent.js
@@ -3,153 +3,43 @@ import "../styles/secondContentStyles.css";
 // codesandbow https://codepen.io/BurmesePotato/pen/qBbqpNB
 const SecondContent = () => {
   return (
-    <div className="second-content">
-      <div class="container">
-        <h1 class="center">Kidnapping The Princess</h1>
-
-        <article class="episode">
-          <div class="episode__number">00</div>
-          <div class="episode__content">
-            <div class="title">Prologue</div>
-            <div class="story">
-              <p>
-                Say hello to the perfection of Cybele- tall, gorgeous,
-                millionaire.
-              </p>
-              <p>
-                Princess Talia was the only successor to the throne, and her
-                absence baffled the kingdom.
-              </p>
-              <p>
-                Watch Cybele, both our hero and villain for the story as she
-                struggles to fulfill her wishes while trying to keep her
-                feelings for the Princess in check.
-              </p>
-            </div>
-          </div>
-        </article>
-
-        <article class="episode">
-          <div class="episode__number">01</div>
-          <div class="episode__content">
-            <div class="title">Episode 1</div>
-            <div class="story">
-              <p>"Let go of me you mongrel!"</p>
-              <p>
-                I watched in fascination as the princess struggled against the
-                ropes binding her ankles and wrist.
-              </p>
-              <p>
-                "Unhand me buffoon and I'll tell my mother the queen to spare
-                your life," she said indignantly.
-              </p>
-              <p>
-                Oooh! How lovely. As if her mother the queen could help her
-                right now.
-              </p>
-              <p>
-                "I doubt your highness will spare my life if I let you go," I
-                said to her.
-              </p>
-              <p>
-                "What do you want from me? Do you want money? Power? Men? I can
-                give you all of that."
-              </p>
-              <p>"Then what do you want?" she asked in a small voice.</p>
-            </div>
-          </div>
-        </article>
-
-        <article class="episode">
-          <div class="episode__number">02</div>
-          <div class="episode__content">
-            <div class="title">Episode 2</div>
-            <div class="story">
-              <p>
-                She froze in an instant and squirmed away from me. "You're a
-                lunatic! Jerk!
-              </p>
-              <p>
-                Her gray eyes took in my features. She licked her thin lips
-                nervously and asked, "What color are your eyes. They are the
-                strangest green."
-              </p>
-              <p>
-                I smirked at her. "They're called forest green princess." Before
-                she could ask another question, I took her in my arms and hauled
-                her to the bedroom.
-              </p>
-              <p>
-                Once we were inside the bedroom, which would be her holding room
-                or her cell if you want to call it that, I set her down on the
-                bed where she squirmed some more.
-              </p>
-              <p>
-                "I am not!" she said heatedly. I clicked my tongue at her. "Sure
-                you're not princess."
-              </p>
-              <p>
-                She stared daggers at me. "Why did you kidnap me? You're a low
-                life! You're the lowest!" she seethed.
-              </p>
-              <p>"Are you going to kill me?" she choked. </p>
-              <p>
-                "If I wanted to, I would have done it a long time ago. So to
-                answer your question, no I'm not going to kill you.
-              </p>
-              <p>
-                I pointed to the windows. "Those are barred. Even if you cut
-                that lovely honey colored hair of yours to make a Rapunzel-like
-                rope, you'll never escape from here."
-              </p>
-            </div>
-          </div>
-        </article>
-
-        <article class="episode">
-          <div class="episode__number">03</div>
-          <div class="episode__content">
-            <div class="title">Episode 3</div>
-            <div class="story">
-              <p>
-                "Scream all you want," I continued. "This hideout is located
-                miles from any town or road. No one will ever hear you.
-              </p>
-              <p>"I despise you," she said savagely. </p>
-              <p>"Well I'm very sorry to hear that," I said with a shrug.</p>
-              <p>
-                "Wait!" she called out. "Aren't you going to remove my
-                restraints?"{" "}
-              </p>
-              <p>
-                Her question made me smile to myself. "Say please," I said. Her
-                jaw clenched. "Are you freaking kidding me?" she said loudly.{" "}
-              </p>
-              <p>
-                "I don't think her highness would appreciate your smart mouth
-                princess." I faced her and grinned widely.
-              </p>
-              <p>
-                I walked back to her and leaned down so I could untie the ropes.
-                "See? I know there's some manners left in you," I teased.
-              </p>
-              <p>
-                My hands deftly removed the knots on the rope. Once she was
-                untied, the princess moved her hands around and sat on the edge
-                of the bed.
-              </p>
-              <p>
-                "You shouldn't have done that," I told her. "My stomach is going
-                to bruise.
-              </p>
-              <p>
-                "I'm warning you princess. If you do that again, I'll rip your
-                clothes off and let you stay here completely naked."
-              </p>
-            </div>
-          </div>
-        </article>
-      </div>
+    <div className="container">
+      <section className="profile">
+        <h4 className="profile-container">
+          <span className="profile-container__title">
+            <span className="profile-container__text">0</span>
+            <span className="profile-container__text">0</span>
+            <span className="profile-container__text">/</span>
+          </span>
+        </h4>
+        <h4 className="profile-intro">
+          <span className="profile-intro-text">ABOUT</span>
+        </h4>
+        <div className="profile-about">
+          <span className="profile-about__intro">
+            CREATING A RICH USER EXPERIENCE THROUGH USER CENTRIC DESIGN IS MY
+            PASSION. I PLAN, DESIGN, AND DEVELOP WITH MODERN TECHNOLOGY,
+            CREATING A MOMENT TO ENJOY.
+          </span>
+        </div>
+        <div className="profile-button">
+          <span className="profile-button__text">More?</span>
+        </div>
+      </section>
+      <section className="projects-container">
+        <h4 className="project-number">
+          <span className="project-title">
+            <span className="project-title__text">0</span>
+            <span className="project-title__text">1</span>
+            <span className="project-title__text">/</span>
+          </span>
+        </h4>
+        <h4 className="project-title">
+          <span className="project-intro">
+            <span className="project-intro-text">PROJECTS</span>
+          </span>
+        </h4>
+      </section>
     </div>
   );
 };
