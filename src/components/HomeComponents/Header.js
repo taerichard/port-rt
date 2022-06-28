12601c91b0fde644b0008de01143bce708e3c754
Richard Tae <taerichard@gmail.com>
2022-06-27 23:47:19 -0700

    refactored second content, created modal, styled images, working on about section

--- src/components/HomeComponents/Header.js
+++ src/components/HomeComponents/Header.js
@@ -2,23 +2,25 @@ import React from "react";
 import "../styles/headerStyles.css";
 import Content from "./Content";
 
-{
-  /* <div className="header__currentRole">
-<a>UI Developer</a>
-</div>
-<div className="header__location">
-<p>
-  Based in Seattle <br /> Washington
-</p>
-</div>
-<div className="header-nav">
-<ul className="header-nav__items">
-  <li className="header-nav__item">Work</li>
-  <li className="header-nav__item">About</li>
-  <li className="header-nav__item">Contact</li>
-</ul>
-</div> */
-}
+const largeHeader = (
+  <div>
+    <div className="header__currentRole">
+      <a>UI Developer</a>
+    </div>
+    <div className="header__location">
+      <p>
+        Based in Seattle <br /> Washington
+      </p>
+    </div>
+    <div className="header-nav">
+      <ul className="header-nav__items">
+        <li className="header-nav__item">Work</li>
+        <li className="header-nav__item">About</li>
+        <li className="header-nav__item">Contact</li>
+      </ul>
+    </div>
+  </div>
+);
 
 const Header = () => {
   return (
--- src/components/HomeComponents/SecondContent.js
+++ src/components/HomeComponents/SecondContent.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/secondContentStyles.css";
 import Image from "./Image";
 import iosImage from "../../images/SecondContent/ios.jpg";
 import ptImage from "../../images/SecondContent/pt.jpg";
-import yelpApp from "../../images/SecondContent/yelpApp.jpg";
+import yelpImage from "../../images/SecondContent/yelpApp.jpg";
+import worldImage from "../../images/SecondContent/world.jpg";
+import Modal from "./Modal";
 
 const SecondContent = () => {
+  // modal
+  const [show, setShow] = useState(false);
+
+  const displayModal = () => {
+    setShow(true);
+  };
+
   return (
     <div className="container">
       <section className="profile">
@@ -16,9 +25,7 @@ const SecondContent = () => {
             <span className="profile-container__text">/</span>
           </span>
         </h4>
-        <h4 className="profile-intro">
-          <span className="profile-intro-text">ABOUT</span>
-        </h4>
+
         <div className="profile-about">
           <span className="profile-about__intro">
             CREATING A RICH USER EXPERIENCE THROUGH USER CENTRIC DESIGN IS MY
@@ -43,10 +50,57 @@ const SecondContent = () => {
             <span className="project-intro-text">PROJECTS</span>
           </span>
         </h4>
-        <div className="project-images">
-          <img className="iosImage" src={iosImage} />
-          <img className="ptImage" src={ptImage} />
-          <img className="yelpImage" src={yelpApp} />
+      </section>
+
+      <section className="project-images">
+        {/* {imageList} */}
+        <div className="image">
+          <span className="circle">
+            <h3 className="image-text" onClick={displayModal}>
+              More
+            </h3>
+          </span>
+          <img className="" src={worldImage} />
+        </div>
+        {/* <div className="modal-container">
+          <Modal
+            open={show}
+            title="Title here"
+            info="info here"
+            role="role here"
+          />
+        </div> */}
+      </section>
+
+      <section className="about">
+        <h4 className="about-number">
+          <span className="about-title__text">002/</span>
+        </h4>
+        <h4 className="about-title">
+          <span className="about-intro-text">ABOUT</span>
+        </h4>
+        <div className="about-points">
+          <span className="about-content__point">HANGING WITH MY DOG.</span>
+          <br />
+          <span className="about-content__point">PHYSICAL FITNESS.</span>
+          <br />
+          <span className="about-content__point">EATING JUNK FOOD.</span>
+          <br />
+          <span className="about-content__point">YOUTUBE MIDDLE BUTTON.</span>
+          <br />
+          <span className="about-content__point">
+            EXPERIENCING NEW CULTURES.
+          </span>
+          <br />
+          <span className="about-content__point">SKIPPING ROCKS.</span>
+        </div>
+        <div className="about-content__point">
+          <span>
+            During my free time, I enjoy a good o'l session of physical fitness.
+            I also train people proper body mechanics through sports specific,
+            strength, and mobility/stability training. I'm either on the
+            computer or running around somewhere.
+          </span>
         </div>
       </section>
     </div>
