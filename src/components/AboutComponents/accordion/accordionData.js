9f77e84da5c8d16a54459a95e9ab001250c12185
Richard Tae <taerichard@gmail.com>
2022-07-09 15:26:57 -0700

    created accordion and connected to about page, added click event and registered, working on styling

---
+++ src/components/AboutComponents/accordion/accordionData.js
@@ -0,0 +1,17 @@
+export const accordionData = [
+  {
+    title: "Design",
+    content:
+      "I plan, design, and test front end applications. I deliver strong and user-friendly designs.",
+  },
+  {
+    title: "Development",
+    content:
+      "I build scalable websites from scratch. My focus is on scalability  and optimizing maximum speed. ",
+  },
+  {
+    title: "Full Package",
+    content:
+      "I plan and develop blueprints for business requirements into applications.",
+  },
+];
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,15 @@ import React from "react";
 import seattleImage from "../images/about/pc.jpg";
 import selfImage from "../images/about/me.jpg";
 import "../components/AboutComponents/styles/about.css";
+import Accordion from "../components/AboutComponents/accordion/Accordion";
+import { accordionData } from "../components/AboutComponents/accordion/accordionData";
 
 const About = () => {
   return (
     <div className="about-container">
       <div className="about-image-container">
-        <h4>001/</h4>
-        <span>Ui developer</span>
+        <h4 className="num">001/</h4>
+        <span className="title">Ui developer</span>
         <div className="about-image__1">
           <img className="" src={seattleImage} />
         </div>
@@ -18,24 +20,32 @@ const About = () => {
           Seattle,WA
         </span>
         <p>
-          I'm a Front-End Developer with a passion for creating immersive user
+          I'm a Front end developer with a passion for creating immersive user
           experiences.
           <br />
           <br />
-          Recently graduated with a Bachelor of Applied Science in Mobile
+          Recently, graduated with a Bachelor of Applied Science in Mobile
           Application Development. I create web experiences through user-centric
           designs. My projects include collaboration with people and independent
           work.
           <br />
           <br />
-          Each project I take it as an opportunity to learn new concepts and
+          Each project, I take it as an opportunity to learn new concepts and
           create lasting relationships.
         </p>
         <div className="about-image__2">
           <img src={selfImage} />
         </div>
       </div>
-      <div className="services-container"></div>
+      <div className="services-container">
+        <h4 className="num">002/</h4>
+        <span className="title">SERVICES</span>
+        <div className="accordion-container">
+          {accordionData.map(({ title, content }) => {
+            return <Accordion title={title} content={content} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
