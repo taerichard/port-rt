cb4b397cb9b6b634085fd68ee68274aba93bc64d
Richard Tae <taerichard@gmail.com>
2022-08-10 16:35:07 -0700

    started work page, researching breaking down list components to single

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="work-container">
+      <div className="work-title-1">
+        <h4>01/</h4>
+      </div>
+      <div className="work-title-2">
+        <h3>projects</h3>
+        <h4>02/</h4>
+      </div>
+      <div className="work-content-title">
+        <h4>projects</h4>
+        <p>
+          Selected projects consisted of collaborative and personal work. These
+          are from clients, personal, and collaborative work.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
