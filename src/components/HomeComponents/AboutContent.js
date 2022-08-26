0ddc8d7cef59d2e75d102efa79bb0a224b40cb40
Richard Tae <taerichard@gmail.com>
2022-08-26 00:08:21 -0700

    working on about section for desktop view. Fixed mobile version by repositioning title, image, and hobbies into one column

--- src/components/HomeComponents/AboutContent.js
+++ src/components/HomeComponents/AboutContent.js
@@ -7,45 +7,42 @@ const AboutContent = () => {
   return (
     <div className="aboutContent-container">
       <div className="aboutContent-title">
-        <h4>03/</h4>
-      </div>
-      <div className="aboutContent-title-2">
-        <h4>ABOUT</h4>
+        <h3>03/</h3>
+        <h4>ABOUT ME</h4>
       </div>
       <div className="aboutContent-title-3">
         <h4>Design & Development</h4>
       </div>
+      <div className="aboutContent-title-4">
+        <h4>ui development</h4>
+      </div>
       <div className="aboutContent-hobbies">
         <h3>travel.</h3>
-        <br />
-        <h3>lifting.</h3>
-        <br />
+
+        <h3>fitness.</h3>
+
         <h3>hiking.</h3>
-        <br />
+
         <h3>music.</h3>
-        <br />
-        <h3>running.</h3>
-        <br />
+
         <h3>food.</h3>
-        <br />
-        <h3>boxing.</h3> <br />
-        <h3>running.</h3> <br />
-        <h3>running.</h3> <br />
       </div>
       <div className="aboutContent-image">
         <img src={travelImage} />
-        <div className="aboutContent-text-1">
-          <p>
-            When I am not web designing, I'm usually doing some kind of fitness,
-            hanging out with my dogs, or eating.
-          </p>
+      </div>
+      <div className="aboutContent-text-1">
+        <p>
+          I like to be active and stay outside when I am not developing. I am a
+          fitness enthusiast and a certified trainer. I enjoy going on hidden
+          trails with my dogs. Oh, and my favorite activity is to eat food.
+        </p>
+        <div className="aboutContent-button">
+          <Link className="aboutContent-button__text" to="about">
+            More
+          </Link>
         </div>
       </div>
-      <div className="aboutContent-button">
-        <Link className="aboutContent-button__text" to="about">
-          More
-        </Link>
-      </div>
+
       <div className="aboutContent-text-2">
         <h3>
           <Link className="aboutContent-text-2__button" to="about">
