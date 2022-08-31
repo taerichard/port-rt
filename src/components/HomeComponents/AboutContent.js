57c0cf7e9e65155b3a657cd318ba9c00c9f2dc56
Richard Tae <taerichard@gmail.com>
2022-08-31 16:28:35 -0700

    touching layout in about component in home page

--- src/components/HomeComponents/AboutContent.js
+++ src/components/HomeComponents/AboutContent.js
@@ -14,18 +14,32 @@ const AboutContent = () => {
         <h4>Design & Development</h4>
       </div>
       <div className="aboutContent-title-4">
-        <h4>ui development</h4>
+        <h4>ui developer</h4>
       </div>
       <div className="aboutContent-hobbies">
-        <h3>travel.</h3>
+        <div className="aboutContent-hobbies1">
+          <h3>travel.</h3>
 
-        <h3>fitness.</h3>
+          <h3>fitness.</h3>
+        </div>
+        <div className="aboutContent-hobbies2">
+          <h3>hiking.</h3>
 
-        <h3>hiking.</h3>
-
-        <h3>music.</h3>
-
-        <h3>food.</h3>
+          <h3>music.</h3>
+        </div>
+        <div className="aboutContent-active__text">
+          <p>
+            I like to be active and stay outside when I am not developing. I am
+            a fitness enthusiast and a certified trainer. I enjoy going on
+            hidden trails with my dogs. Oh, and my favorite activity is to eat
+            food.
+          </p>
+          <h3>
+            <Link className="aboutContent-text-2__button" to="about">
+              More About Me
+            </Link>
+          </h3>
+        </div>
       </div>
       <div className="aboutContent-image">
         <img src={travelImage} />
@@ -41,14 +55,6 @@ const AboutContent = () => {
             More
           </Link>
         </div>
-      </div>
-
-      <div className="aboutContent-text-2">
-        <h3>
-          <Link className="aboutContent-text-2__button" to="about">
-            More About Me
-          </Link>
-        </h3>
       </div>
     </div>
   );
