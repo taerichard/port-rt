9085aabde2d11cfc5251112e846e61dc0751834b
Richard Tae <taerichard@gmail.com>
2022-09-12 03:51:50 -0700

    realigned contact heading

--- src/components/HomeComponents/ContactContent.js
+++ src/components/HomeComponents/ContactContent.js
@@ -6,20 +6,14 @@ const ContactContent = () => {
     <div className="contactContent-container">
       <div className="contactContent-title">
         <h3>04/</h3>
-        <h4>
-          contact <br />
-          me
-        </h4>
+        <h4>contact </h4>
       </div>
       <div className="contactContent-title-2">
         <h4>Design & Development</h4>
       </div>
 
       <div className="contactContent-title-3">
-        <h4>
-          ui <br />
-          developer
-        </h4>
+        <h4>ui developer</h4>
       </div>
       <div className="contactContent-contact">
         <a target="_blank" href="mailto:taerichard@gmail.com">
